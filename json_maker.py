21d22d2619fc04bf69e6ef6d9709627f56700348
Kazutadashi <Kazutadashi@yahoo.com>
2021-07-30 01:20:00 -0600

    downloaded more data

--- json_maker.py
+++ json_maker.py
@@ -7,15 +7,14 @@ import json
 import urllib.request
 import time
 
+
 PLAYER_LIST_PATH = "player_list_june_2021.csv"
-OUTPUT_FILENAME = "player_data_june_2021_0-50000.json"
+OUTPUT_FILENAME = "player_data_june_2021_150000-200000.json"
 
 # opens csv file to load in players
 with open(PLAYER_LIST_PATH, newline='') as f:
     reader = csv.reader(f)
     players = list(reader)
-
-#print(players[0])
 
 # first creates a file that starts with a JSON list structure
 with open(OUTPUT_FILENAME, "w") as player_data_json:
@@ -23,7 +22,7 @@ with open(OUTPUT_FILENAME, "w") as player_data_json:
 
 # Gets json data for each player, in this specific [0][0:1000] we just want 1000 players to test
 # uses tqdm for a progress bar
-for player in tqdm.tqdm(players[0][0:50000]):
+for player in tqdm.tqdm(players[0][150000:200000]):
     try:
         #print("Fetching JSON data for " + player + "...")
         with urllib.request.urlopen("https://lichess.org/api/user/" + player) as url:
